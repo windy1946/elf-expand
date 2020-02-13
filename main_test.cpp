e74fee9ef2eaa3f7c2cb92df83f4ca749b7d3b2d
yao.mai <yao.mai@dingxiang-inc.com>
2020-02-13 18:59:31 +0800

    编译通过，能够通过参数控制patch的so

--- main_test.cpp
+++ main_test.cpp
@@ -1,12 +1,17 @@
-#include <iostream>
 #include "expand_elf.h"
+#include <stdio.h>
 
-int main(){
-    const char* input_so = "libtest.so";
-    const char* output_so = "libtest-o.so";
+int main(int argc, char **argv){
+	if (argc < 4) {
+		printf("usage %s <input_so> <output_so> <n_expand_hex>\n", argv[0]);
+		return -1;
+	}
+    const char* input_so = argv[1];
+    const char* output_so = argv[2];
     expand_elf elf(input_so, output_so);
-
-    elf.expand(0x4000);
+	const char *str_n_expand = argv[3];
+	int n_expand = strtol(str_n_expand, 0, 16);
+    elf.expand(n_expand);
     
     return 0;
 }
